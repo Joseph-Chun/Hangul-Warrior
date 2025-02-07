24d4fd85af617adc00c6ddfb5fb93a830f35ef1e
Joseph-Chun <tianbingwu@naver.com>
2025-02-07 11:28:37 +0900

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import ScoreCounter from '@/components/ScoreCounter'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <ScoreCounter />
+    </main>
+  )
+}
